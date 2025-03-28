9b5f502329202a9bb868c091dae6f2c41c2e0190
Salman ahmed <salman.ahmed.sal4slm@gmail.com>
2025-03-29 04:46:22 +0600

    added pic adn changed bg to yt vid

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// src/global.d.ts
+interface Window {
+  dataLayer: any[];
+  gtag: (...args: any[]) => void;
+}
